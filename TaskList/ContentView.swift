81d3d15215b79750c3249eeb4298bdb9d9739e1f
Vinh Le <vinh@vertics.co>
2019-12-03 23:55:20 +0200

    Initial Commit

---
+++ TaskList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TaskList
+//
+//  Created by Vinh Le on 12/3/19.
+//  Copyright © 2019 Vinh Le. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
